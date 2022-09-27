375d3f5438b0f5e2d6c3ab5e0b08dafadf0eb817
andrewebb91 <andrewebb91@gmail.com>
2022-09-26 19:11:35 -0500

    views

--- components/Views/Home.js
+++
@@ -1,8 +0,0 @@
-import html from "html-literal";
-
-export default (state) => html`
-  <section id="jumbotron">
-    <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html">"Call to Action" "Button"</a>
-  </section>
-`;
---
+++ components/d/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export {default as Bio} from "./Bio";
+export {default as Home} from "./Home";
+export {default as Order} from "./Order";
+export {default as Pizza} from "./Pizza";
